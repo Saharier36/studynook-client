85728ccffb1b425f078a16b89d0297a9b494d862
Saharier Omi <gsaharier761@gmail.com>
2026-05-22 21:48:10 +0600

    Add font files

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,33 @@ import Footer from "@/components/layout/Footer";
 import "./globals.css";
 import { Providers } from "./providers";
 import { Toaster } from "sonner";
+import localFont from "next/font/local";
+
+const technor = localFont({
+  src: [
+    { path: "../../fonts/Technor-Extralight.woff2", weight: "200" },
+    { path: "../../fonts/Technor-Light.woff2", weight: "300" },
+    { path: "../../fonts/Technor-Regular.woff2", weight: "400" },
+    { path: "../../fonts/Technor-Medium.woff2", weight: "500" },
+    { path: "../../fonts/Technor-Semibold.woff2", weight: "600" },
+    { path: "../../fonts/Technor-Bold.woff2", weight: "700" },
+    { path: "../../fonts/Technor-Black.woff2", weight: "900" },
+  ],
+  variable: "--font-technor",
+});
+
+const supreme = localFont({
+  src: [
+    { path: "../../fonts/Supreme-Thin.woff2", weight: "100" },
+    { path: "../../fonts/Supreme-Extralight.woff2", weight: "200" },
+    { path: "../../fonts/Supreme-Light.woff2", weight: "300" },
+    { path: "../../fonts/Supreme-Regular.woff2", weight: "400" },
+    { path: "../../fonts/Supreme-Medium.woff2", weight: "500" },
+    { path: "../../fonts/Supreme-Bold.woff2", weight: "700" },
+    { path: "../../fonts/Supreme-Extrabold.woff2", weight: "800" },
+  ],
+  variable: "--font-supreme",
+});
 
 export const metadata = {
   title: "StudyNook - Library Study Room Booking",
@@ -11,14 +38,11 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className="h-full antialiased" suppressHydrationWarning>
-      <head>
-        <link
-          href="https://api.fontshare.com/v2/css?f[]=technor@400,500,600,700&f[]=supreme@400,500,700&display=swap"
-          rel="stylesheet"
-          crossOrigin="anonymous"
-        />
-      </head>
+    <html
+      lang="en"
+      className={`h-full antialiased ${technor.variable} ${supreme.variable}`}
+      suppressHydrationWarning
+    >
       <body className="min-h-full flex flex-col overflow-x-hidden">
         <Providers>
           <Toaster richColors position="top-right" />
